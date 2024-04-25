22d602912cda2050c794f69c782dbd7e1b524a78
lambiengcode <lambiengcode@gmail.com>
2024-04-25 15:32:33 +0700

    update .

--- call/rtp_payload_params.cc
+++ call/rtp_payload_params.cc
@@ -421,6 +421,7 @@ absl::optional<FrameDependencyStructure> RtpPayloadParams::GenericStructure(
     case VideoCodecType::kVideoCodecAV1:
     case VideoCodecType::kVideoCodecH264:
     case VideoCodecType::kVideoCodecH265:
+    case VideoCodecType::kVideoCodecMultiplex:
       return absl::nullopt;
   }
   RTC_DCHECK_NOTREACHED() << "Unsupported codec.";
--- media/engine/simulcast_encoder_adapter.h
+++ media/engine/simulcast_encoder_adapter.h
@@ -43,16 +43,6 @@ namespace webrtc {
 // interfaces should be called from the encoder task queue.
 class RTC_EXPORT SimulcastEncoderAdapter : public VideoEncoder {
  public:
-  // TODO(bugs.webrtc.org/11000): Remove when downstream usage is gone.
-  SimulcastEncoderAdapter(VideoEncoderFactory* primarty_factory,
-                          const SdpVideoFormat& format);
-
-  // `primary_factory` produces the first-choice encoders to use.
-  // `fallback_factory`, if non-null, is used to create fallback encoder that
-  // will be used if InitEncode() fails for the primary encoder.
-  SimulcastEncoderAdapter(VideoEncoderFactory* primary_factory,
-                          VideoEncoderFactory* fallback_factory,
-                          const SdpVideoFormat& format);
   // `primary_factory` produces the first-choice encoders to use.
   // `fallback_factory`, if non-null, is used to create fallback encoder that
   // will be used if InitEncode() fails for the primary encoder.
