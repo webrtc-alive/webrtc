8923e2be842a6613a3ff4eff7d6c699e7fb47e8f
lambiengcode <lambiengcode@gmail.com>
2024-05-31 15:18:28 +0700

    remove initWithNativePeerConnectionFactory

--- sdk/objc/api/peerconnection/RTCPeerConnectionFactory+Native.h
+++ sdk/objc/api/peerconnection/RTCPeerConnectionFactory+Native.h
@@ -11,7 +11,6 @@
 #import "RTCPeerConnectionFactory.h"
 
 #include "api/scoped_refptr.h"
-#include "api/peer_connection_interface.h"
 
 namespace webrtc {
 
@@ -74,8 +73,6 @@ NS_ASSUME_NONNULL_BEGIN
     initWithEncoderFactory:(nullable id<RTC_OBJC_TYPE(RTCVideoEncoderFactory)>)encoderFactory
             decoderFactory:(nullable id<RTC_OBJC_TYPE(RTCVideoDecoderFactory)>)decoderFactory;
 
-
-
 /** Initialize an RTCPeerConnection with a configuration, constraints, and
  *  dependencies.
  */
@@ -84,14 +81,6 @@ NS_ASSUME_NONNULL_BEGIN
                        constraints:(RTC_OBJC_TYPE(RTCMediaConstraints) *)constraints
                       dependencies:(std::unique_ptr<webrtc::PeerConnectionDependencies>)dependencies
                           delegate:(nullable id<RTC_OBJC_TYPE(RTCPeerConnectionDelegate)>)delegate;
-
-
-- (instancetype)initWithNativePeerConnectionFactory:(
-    rtc::scoped_refptr<webrtc::PeerConnectionFactoryInterface>)factory;
-
-
-- (instancetype)initWithNativePeerConnectionFactory:(
-    rtc::scoped_refptr<webrtc::PeerConnectionFactoryInterface>)factory;
 
 @end
 
